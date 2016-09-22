869be889d64e7afd0b742135e0ebf30d660a5147
suhridsatyal <suhridsatyal@gmail.com>
2016-09-22 12:58:37 +1000

    Refactors Question View

--- stack_annotator/assets/app/scripts/views/question.js
+++ stack_annotator/assets/app/scripts/views/question.js
@@ -11,214 +11,267 @@ define([
   'text!../templates/tooltip_menu.html',
   'text!../templates/annotations.html'
 ], function($, _, Backbone,
-            QuestionModel, AnswerCollection, AnnotationCollection,
-            questionTemplate, tooltipTemplate, annotationsTemplate){
+  QuestionModel, AnswerCollection, AnnotationCollection,
+  questionTemplate, tooltipTemplate, annotationsTemplate) {
 
   var QuestionView = Backbone.View.extend({
-      el: $('.container'),
-      initialize: function(options) {
-          this.options = options || {};
-          this.options.selectedText = "";
-      },
-      render: function() {
-          var self = this;
-          self.options = this.options;
-          var data = {};
+    el: $('.container'),
 
-          var question = new QuestionModel({post: this.options.post});
-          var answers = new AnswerCollection({post: this.options.post});
-          var annotations = new AnnotationCollection();
-          $.when(question.fetch(),
-                 answers.fetch(),
-                 annotations.fetch({data: {'question_id': self.options.post}}))
-            .done(function () {
-              data.question = question.get("title");
-              data.questionBody = question.get("body");
-              var annotationList = annotations.toJSON();
-              var annotatedAnswers = self.annotateAnswers(answers.toJSON(), annotationList);
-              data.answers = self.sortAnswers(annotatedAnswers);
+    initialize: function(options) {
+      this.options = options || {};
+      this.options.selectedText = "";
+    },
 
-              var compiledTemplate = _.template(questionTemplate);
-              self.$el.empty().append(compiledTemplate(data));
-              if (self.options.answerID) {
-                  // Scroll to answer
-                  var answerElem = "#" + self.options.answerID;
-                  var answerElemOffset = $(answerElem).offset();
-                  $('html,body').animate({scrollTop: answerElemOffset.top}, "fast");
+    render: function() {
+      var self = this;
+      self.options = this.options;
+      var data = {};
 
-                  if (self.options.highlightType == 'annotation') {
-                      // Scroll to annotation
-                      var annotationElem = "#" + self.options.highlightID + ".annotation_text";
-                      var annotationElemOffset = $(annotationElem).offset();
-                      $('html,body').animate({scrollTop: annotationElemOffset.top}, "fast");
+      var question = new QuestionModel({
+        post: this.options.post
+      });
+      var answers = new AnswerCollection({
+        post: this.options.post
+      });
+      var annotations = new AnnotationCollection();
+      $.when(question.fetch(),
+          answers.fetch(),
+          annotations.fetch({
+            data: {
+              'question_id': self.options.post
+            }
+          }))
+        .done(function() {
+          data.question = question.get("title");
+          data.questionBody = question.get("body");
+          var annotationList = annotations.toJSON();
+          var annotatedAnswers = self._annotateAnswers(answers.toJSON(), annotationList);
+          data.answers = self._sortAnswers(annotatedAnswers);
 
-                      self.showYoutubeURL(self.options.highlightID, annotations);
-                  } else if (self.options.highlightType == 'task') {
-                      // Scroll to Task
-                      var annotationElem = "#" + self.options.highlightID + ".task_text";
-                      var annotationElemOffset = $(annotationElem).offset();
-                      $('html,body').animate({scrollTop: annotationElemOffset.top}, "fast");
-                      // Show comment box
-                      // TODO
-                  }
-              }
-          });
-          this.annotations = annotations;
-   },
+          var compiledTemplate = _.template(questionTemplate);
+          self.$el.empty().append(compiledTemplate(data));
+          if (self.options.answerID) {
+            // Scroll to answer
+            var answerElem = "#" + self.options.answerID;
+            var answerElemOffset = $(answerElem).offset();
+            $('html,body').animate({
+              scrollTop: answerElemOffset.top
+            }, "fast");
 
-   //
-   // Events and Listeners
-   //
-   events: {
-       'mouseup #answers': 'onHighlight',
-       'mousedown #questionview': 'onDeselect',
-       'mouseover .annotation_text': 'onAnnotationHover'
-   },
-   onHighlight: function(){
-     var rects = [];
-     var selectedText = "";
+            if (self.options.highlightType == 'annotation') {
+              // Scroll to annotation
+              var annotationElem = "#" + self.options.highlightID + ".annotation_text";
+              var annotationElemOffset = $(annotationElem).offset();
+              $('html,body').animate({
+                scrollTop: annotationElemOffset.top
+              }, "fast");
 
-     if (window.getSelection) {
-       selectedText = window.getSelection().toString();
-       var box = window.getSelection().getRangeAt(0).getBoundingClientRect();
-       rects = this.getHighlightOffset(box);
-     } else if (document.selection) {
-       selectedText = document.getSelection().toString();
-       var box = document.getSelection().getRangeAt(0).getBoundingClientRect();
-       rects = this.getHighlightOffset(box);
-     } else {
-       return;
-     }
+              self._showYoutubeURL(self.options.highlightID, annotations);
+            } else if (self.options.highlightType == 'task') {
+              // Scroll to Task
+              var annotationElem = "#" + self.options.highlightID + ".task_text";
+              var annotationElemOffset = $(annotationElem).offset();
+              $('html,body').animate({
+                scrollTop: annotationElemOffset.top
+              }, "fast");
+              // Show comment box
+              // TODO
+            }
+          }
+        });
+      this.annotations = annotations;
+    },
 
-     this.options.selectedText = selectedText;
-     var self=this;
+    //
+    // Events and Listeners
+    //
+    events: {
+      'mouseup #answers': 'onHighlight',
+      'mousedown #questionview': 'onDeselect',
+      'mouseover .annotation_text': 'onAnnotationHover'
+    },
 
-     if (selectedText.length > 0) {
-       this.cleanupPopover();
+    onHighlight: function() {
+      var rects = [];
+      var selectedText = "";
 
-       //show popover
-       console.log(tooltipTemplate);
-       $("#annotate-tooltip").popover({
-           trigger: 'focus',
-           container: 'body',
-           //placement: 'bottom',
-           content: function() {
-               return tooltipTemplate;
-           },
-           html: true
-       }).popover('show');
-       $(".popover").css({top: rects.bottom, left: rects.left, transform: ''}).show();
+      if (window.getSelection) {
+        selectedText = window.getSelection().toString();
+        var box = window.getSelection().getRangeAt(0).getBoundingClientRect();
+        rects = this._getHighlightOffset(box);
+      } else if (document.selection) {
+        selectedText = document.getSelection().toString();
+        var box = document.getSelection().getRangeAt(0).getBoundingClientRect();
+        rects = this._getHighlightOffset(box);
+      } else {
+        return;
+      }
 
-       // Attach events to popover buttons.
-       $("#crowdsourceBtn").on("click", function(event) {self.onCrowdsource()});
-       $("#commentBtn").on("click", function(event) {self.onComment()});
-       $("#helpBtn").on("click", function(event) {self.onHelp()});
-     };
-   },
-   onDeselect: function() {
-     console.log("Deselect!");
-     this.cleanupPopover();
-   },
-   onAnnotationHover:function(evt) {
-       this.showYoutubeURL(evt.target.id, this.annotations);
-   },
-   onCrowdsource: function () {
-       console.log("TODO: call backend to create crowdsourcing request");
-       console.log("Selected Text: " + this.options.selectedText);
-   },
-   onComment: function () {
-       console.log("TODO: allow users to comment");
-   },
-   onHelp: function () {
-       console.log("TODO: show help");
-   },
+      this.options.selectedText = selectedText;
+      var self = this;
 
-   //
-   // Helper Functions
-   //
-   getHighlightOffset: function(box) {
-     /* Returns offset of selected text.
-      * Code adapted from this tutorial:
-      * http://javascript.info/tutorial/coordinates
-      */
-     var body = document.body;
-     var docElem = document.documentElement;
+      if (selectedText.length > 0) {
+        this._cleanupPopover();
 
-     var scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop;
-     var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft;
+        //show popover
+        console.log(tooltipTemplate);
+        $("#annotate-tooltip").popover({
+          trigger: 'focus',
+          container: 'body',
+          //placement: 'bottom',
+          content: function() {
+            return tooltipTemplate;
+          },
+          html: true
+        }).popover('show');
+        $(".popover").css({
+          top: rects.bottom,
+          left: rects.left,
+          transform: ''
+        }).show();
 
-     var clientTop = docElem.clientTop || body.clientTop || 0;
-     var clientLeft = docElem.clientLeft || body.clientLeft || 0;
+        // Attach events to popover buttons.
+        $("#crowdsourceBtn").on("click", function(event) {
+          self.onCrowdsource();
+        });
+        $("#commentBtn").on("click", function(event) {
+          self.onComment();
+        });
+        $("#helpBtn").on("click", function(event) {
+          self.onHelp();
+        });
+      };
+    },
 
-     var bottom = box.bottom +  scrollTop - clientTop;
-     var left = box.left + scrollLeft - clientLeft;
-     var right = box.right + scrollLeft - clientLeft;
+    onDeselect: function() {
+      this._cleanupPopover();
+    },
+
+    onAnnotationHover: function(evt) {
+      this._showYoutubeURL(evt.target.id, this.annotations);
+    },
+
+    onCrowdsource: function() {
+      console.log("TODO: call backend to create crowdsourcing request");
+      console.log("Selected Text: " + this.options.selectedText);
+    },
+
+    onComment: function() {
+      console.log("TODO: allow users to comment");
+    },
+
+    onHelp: function() {
+      console.log("TODO: show help");
+    },
+
+    //
+    // Helper Functions
+    //
+    _getHighlightOffset: function(box) {
+      /* Returns offset of selected text.
+       * Code adapted from this tutorial:
+       * http://javascript.info/tutorial/coordinates
+       */
+      var body = document.body;
+      var docElem = document.documentElement;
+
+      var scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop;
+      var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft;
+
+      var clientTop = docElem.clientTop || body.clientTop || 0;
+      var clientLeft = docElem.clientLeft || body.clientLeft || 0;
+
+      var bottom = box.bottom + scrollTop - clientTop;
+      var left = box.left + scrollLeft - clientLeft;
+      var right = box.right + scrollLeft - clientLeft;
 
 
-     return { bottom: Math.round(bottom), left: Math.round(left), right: Math.round(right) };
-   },
-   showYoutubeURL: function(annotationID, annotations){
+      return {
+        bottom: Math.round(bottom),
+        left: Math.round(left),
+        right: Math.round(right)
+      };
+    },
+
+    _showYoutubeURL: function(annotationID, annotations) {
       // Shows Annotations (Youtube URLS) next to highlighted text
-      this.cleanupPopover();
+      this._cleanupPopover();
       var annotationElem = "#" + annotationID + ".annotation_text";
       var annotationElemOffset = $(annotationElem).offset();
       annotationElemOffset.bottom = annotationElemOffset.top + $(annotationElem).outerHeight(true);
       annotationElemOffset.right = annotationElemOffset.left + $(annotationElem).outerWidth(true);
-      var youtubeURL =  annotations.get(annotationID).
-                      get('annotation').
-                      replace("watch?v=", "embed/");
 
-      var annotationData = {id: annotationID,
-                          annotation: youtubeURL };
+      var youtubeURL = annotations.get(annotationID).
+          get('annotation').
+          replace("watch?v=", "embed/");
+      var annotationData = {
+        id: annotationID,
+        annotation: youtubeURL
+      };
       var annotationLinks = _.template(annotationsTemplate)(annotationData);
       $("#annotate-tooltip").popover({
-          trigger: 'focus',
-          container: 'body',
-          placement: 'right',
-          content: function() {
-              return annotationLinks;
-          },
-          html: true
+        trigger: 'focus',
+        container: 'body',
+        placement: 'right',
+        content: function() {
+          return annotationLinks;
+        },
+        html: true
       }).popover('show');
-      $(".popover").css({top: annotationElemOffset.top, left: annotationElemOffset.right, 'max-width': '640px', transform: ''}).show();
-   },
-   annotateAnswers: function(answers, annotations){
-       // Surrounds annotated text with span, with id=annotation id
-       _.each(annotations, function(annotation) {
-         _.each(answers, function(answer) {
-            if (answer.answer_id == annotation.answer_id) {
-              var replacer = function(match, offset) {
-                  if (offset==annotation.position) {
-                      return "<span class='highlighted annotation_text' id=" + annotation.id + ">" + match + "</span>";
-                  } else {
-                      return match;
-                  }
+
+      $(".popover").css({
+        top: annotationElemOffset.top,
+        left: annotationElemOffset.right,
+        'max-width': '640px',
+        transform: ''
+      }).show();
+    },
+
+    _annotateAnswers: function(answers, annotations) {
+      // Surrounds annotated text with span, with id=annotation id
+      _.each(annotations, function(annotation) {
+        _.each(answers, function(answer) {
+          if (answer.answer_id == annotation.answer_id) {
+            var replacer = function(match, offset) {
+              if (offset == annotation.position) {
+                return "<span class='highlighted annotation_text' id=" + annotation.id + ">" + match + "</span>";
+              } else {
+                return match;
               }
-              answer.body = answer.body.replace(annotation.keyword, replacer);
-           }
-         });
-       });
-       return answers;
-   },
-   sortAnswers: function(unsortedAnswers) {
-        var sortedEntries = [];
+            };
+            answer.body = answer.body.replace(annotation.keyword, replacer);
+          }
+        });
+      });
+      return answers;
+    },
 
-        var acceptedAnswer = _.find(unsortedAnswers, function(answer){return answer.is_accepted==true});
-        var otherAnswers = _.reject(unsortedAnswers, function(answer){return answer.is_accepted==true});
+    _sortAnswers: function(unsortedAnswers) {
+      var sortedEntries = [];
 
-        if (!(typeof acceptedAnswer == "undefined")) {
-            sortedEntries.push(acceptedAnswer);
-        };
-        var sortedAnswers = sortedEntries.concat(
-            _.sortBy(otherAnswers, function(answer) {return -answer.score;}));
+      var acceptedAnswer = _.find(unsortedAnswers, function(answer) {
+        return answer.is_accepted == true;
+      });
+      var otherAnswers = _.reject(unsortedAnswers, function(answer) {
+        return answer.is_accepted == true;
+      });
 
-        return sortedAnswers;
-   },
-   cleanupPopover: function() {
-       $(".popover").remove();
-       $("#annotate-tooltip").remove();
-       $("#questionview").append('<div id="annotate-tooltip" data-toggle="popover"> </div>');
-   }
+      if (!(typeof acceptedAnswer == "undefined")) {
+        sortedEntries.push(acceptedAnswer);
+      };
+      var sortedAnswers = sortedEntries.concat(
+        _.sortBy(otherAnswers, function(answer) {
+          return -answer.score;
+        }));
+
+      return sortedAnswers;
+    },
+
+    _cleanupPopover: function() {
+      $(".popover").remove();
+      $("#annotate-tooltip").remove();
+      $("#questionview").append('<div id="annotate-tooltip" data-toggle="popover"> </div>');
+    }
   });
 
   return QuestionView;
