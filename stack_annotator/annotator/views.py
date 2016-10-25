bc94c493fe8a060aea1f61854bde721c3ec92b92
Geoffrey <fasor@outlook.com>
2016-10-26 07:56:44 +1100

    Removed extra migrations

--- stack_annotator/annotator/views.py
+++ stack_annotator/annotator/views.py
@@ -259,6 +259,7 @@ class TaskListView(APIView):
         newAnnotation.save()
 
         # append and shorten url
+        """
         appended_url = request.data['annotation_url'] + "/" \
                                                       + str(newAnnotation.id) \
                                                       + "?taskType=" \
@@ -295,10 +296,11 @@ class TaskListView(APIView):
             # remove annotation we just created
             newAnnotation.delete()
             return Response(errorMsg, status=status.HTTP_400_BAD_REQUEST)
-
+        """
         # create a new task
         task = Task()
-        task.tweet_id = tweet_info['id']
+        #task.tweet_id = tweet_info['id']
+        task.tweet_id = 5
         task.annotation_id = newAnnotation.id
         task.task_type = taskType
         task.created_on = task.checked_on = timezone.now()
