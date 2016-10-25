a92fb4c5b04a84a840781dbfd8307966b1a9ab97
suhridsatyal <suhridsatyal@gmail.com>
2016-10-26 10:01:19 +1100

    Moves migration and collectstatic to init script

--- devops/container_init.sh
+++ devops/container_init.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+etc/init.d/postgresql start
+
+
+cd /opt/StackAnnotator/stack_annotator/ && \
+   python manage.py migrate && \
+   python manage.py collectstatic --noinput
+
+cd /opt/StackAnnotator/stack_annotator/assets/ && \
+   make
+
+cd /opt/StackAnnotator/
+
 service nginx start
 supervisord
-etc/init.d/postgresql start
 
 # Ensure that container does not exit
 /bin/bash
